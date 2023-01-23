e2414d77cdbf15f2f4509ae27fd1cf3eed75ca15
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2023-01-24 06:05:05 +0700

    2023 01 24 6:04

--- function.php
+++ function.php
@@ -49,3 +49,88 @@ function Redirect($link, $notif) {
 function Dompdf(){
     require_once 'assets/dompdf/autoload.inc.php';
 }
+
+function BulanIndonesia($date_month_hire = string){
+     //Bulan Indonesia
+     if($date_month_hire == '01'){
+        $month_indo_hire = 'Januari';
+    }elseif($date_month_hire == '02'){
+        $month_indo_hire = 'Februari';            
+    }elseif($date_month_hire == '03'){
+        $month_indo_hire = 'Maret';            
+    }elseif($date_month_hire == '04'){
+        $month_indo_hire = 'April';            
+    }elseif($date_month_hire == '05'){
+        $month_indo_hire = 'Mei';            
+    }elseif($date_month_hire == '06'){
+        $month_indo_hire = 'Juni';            
+    }elseif($date_month_hire == '07'){
+        $month_indo_hire = 'Juli';            
+    }elseif($date_month_hire == '08'){
+        $month_indo_hire = 'Agustus';            
+    }elseif($date_month_hire == '09'){
+        $month_indo_hire = 'September';            
+    }elseif($date_month_hire == '10'){
+        $month_indo_hire = 'Oktober';            
+    }elseif($date_month_hire == '11'){
+        $month_indo_hire = 'November';            
+    }elseif($date_month_hire == '12'){
+        $month_indo_hire = 'Desember';            
+    }
+
+    return  $month_indo_hire;
+}
+
+function TanggalIndonesia($tanggal = string){
+    $date_hire = new \DateTime($tanggal .' 00:00:00');
+    $date_year_hire = date_format($date_hire, "Y"); //for Display Year
+    $date_month_hire =  date_format($date_hire, "m"); //for Display Month
+    $date_day_hire = date_format($date_hire, "d"); //for Display Date
+
+    $day_hire = gmdate("l", mktime(0,0,0, $date_day_hire,$date_month_hire,$date_year_hire));
+
+    // Hari Indonesia
+    if($day_hire == 'Monday'){
+        $day_indo_hire = 'Senin';
+    }elseif($day_hire == 'Tuesday'){
+        $day_indo_hire = 'Selasa';            
+    }elseif($day_hire == 'Wednesday'){
+        $day_indo_hire = 'Rabu';            
+    }elseif($day_hire == 'Thursday'){
+        $day_indo_hire = 'Kamis';            
+    }elseif($day_hire == 'Friday'){
+        $day_indo_hire = 'Jumat';            
+    }elseif($day_hire == 'Saturday'){
+        $day_indo_hire = 'Sabtu';            
+    }elseif($day_hire == 'Sunday'){
+        $day_indo_hire = 'Minggu';            
+    }
+
+    //Bulan Indonesia
+    if($date_month_hire == '01'){
+        $month_indo_hire = 'Januari';
+    }elseif($date_month_hire == '02'){
+        $month_indo_hire = 'Februari';            
+    }elseif($date_month_hire == '03'){
+        $month_indo_hire = 'Maret';            
+    }elseif($date_month_hire == '04'){
+        $month_indo_hire = 'April';            
+    }elseif($date_month_hire == '05'){
+        $month_indo_hire = 'Mei';            
+    }elseif($date_month_hire == '06'){
+        $month_indo_hire = 'Juni';            
+    }elseif($date_month_hire == '07'){
+        $month_indo_hire = 'Juli';            
+    }elseif($date_month_hire == '08'){
+        $month_indo_hire = 'Agustus';            
+    }elseif($date_month_hire == '09'){
+        $month_indo_hire = 'September';            
+    }elseif($date_month_hire == '10'){
+        $month_indo_hire = 'Oktober';            
+    }elseif($date_month_hire == '11'){
+        $month_indo_hire = 'November';            
+    }elseif($date_month_hire == '12'){
+        $month_indo_hire = 'Desember';            
+    }
+    return  $date_day_hire. " ". $month_indo_hire . " ". $date_year_hire;
+}
--- views/laporan/distribusi/distribusi_cetak.php
+++ views/laporan/distribusi/distribusi_cetak.php
@@ -30,142 +30,194 @@
         height: 90px;
     }
 </style>
+<?php
+if (isset($_GET['tanggal_awal']) && isset($_GET['tanggal_akhir'])) {
+    $sql_peninjauan = "SELECT * FROM peninjauan WHERE tanggal_peninjauan BETWEEN '" . $_GET['tanggal_awal'] . "' AND '" . $_GET['tanggal_akhir'] . "' ";
+    $sql_distribusi = "SELECT * FROM distribusi WHERE tanggal_distribusi BETWEEN '" . $_GET['tanggal_awal'] . "' AND '" . $_GET['tanggal_akhir'] . "' ";
+    $hal = "dari tanggal ". $_GET['tanggal_awal']. " Sampai ".$_GET['tanggal_akhir'];
+} elseif (isset($_GET['bulan']) && isset($_GET['tahun'])) {
+    $sql_peninjauan = "SELECT * FROM peninjauan WHERE MONTH(tanggal_peninjauan) = " . $_GET['bulan'] . " AND YEAR(tanggal_peninjauan) = '" . $_GET['tahun'] . "' ";
+    $sql_distribusi = "SELECT * FROM distribusi WHERE MONTH(tanggal_distribusi) = " . $_GET['bulan'] . " AND YEAR(tanggal_distribusi) = '" . $_GET['tahun'] . "' ";
+    $hal = " bulan ". BulanIndonesia($_GET['bulan'])." Tahun ". $_GET['tahun'];
+} elseif (isset($_GET['tahun'])) {
+    $sql_peninjauan = "SELECT * FROM peninjauan WHERE  YEAR(tanggal_peninjauan) = '" . $_GET['tahun'] . "' ";
+    $sql_distribusi = "SELECT * FROM distribusi WHERE  YEAR(tanggal_distribusi) = '" . $_GET['tahun'] . "' ";
+    $hal = " Tahun ". $_GET['tahun'];
+} else {
+    $sql_peninjauan = "SELECT * FROM peninjauan";
+    $sql_distribusi = "SELECT * FROM distribusi";
+    $hal = "";
+}
+
+$Penanganan_kedaruratan = NumRows($sql_peninjauan);
+$Bantuan_logistik = NumRows($sql_distribusi);
+
+?>
 
 <body>
-
     <div style="text-align:center;">
         <table style="width:100%; border: none;">
             <tr>
                 <td rowspan="4">
-                    <img src="<?= $url ?>/assets/images/bpbdkudus.png" alt="" id="imglogo">
+                    <img src="<?= $url ?>/assets/images/logo-kab-kudus.png" alt="" id="imglogo">
                 </td>
                 <td>
                     PEMERINTAH KABUPATEN KUDUS
                 </td>
             </tr>
             <tr>
-                <td>Jl. PG. Rendeng, Mlatinorowito Telp / Faxs. (0291) 4250022 Kudus 59313</td>
+                <td>
+                    <span style="font-size: 23px;">
+                        BADAN PENANGGULANGAN BENCANA DAERAH
+                    </span>
+                </td>
             </tr>
             <tr>
-                <td>BADAN PENANGGULANGAN BENCANA DAERAH</td>
+                <td>Jl. PG. Rendeng, Mlatinorowito Kudus 59313 Telp./Faxs. (0291) 4250022 </td>
             </tr>
-            <tr>
-                <td>E-mail : bpbdkudus_jateng@yahoo.com </td>
+            <tr style="margin: 0%;">
+                <td>E-mail: bpbdkudus_jateng@yahoo.com</td>
+            </tr>
+            <tr style="margin: 0%;">
+                <td colspan="2" r>
+                    <hr style="position: relative;  top: 20px; height: 3px; background: black; border: 1px solid black;  ">
+                </td>
             </tr>
             <table>
     </div>
-    <h4 class="text-center  ">LAPORAN DATA PENDISTRIBUSIAN</h4>
-
-    <div style="align-content: center;">
-        <table class="table table-striped" id="table_data">
-            <thead>
-                <tr>
-                    <th>Pengatur Distribusi</th>
-                    <th>Tanggal</th>
-                    <th>Bencana</th>
-                    <th>Wilayah</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                if (isset($_GET['tanggal_awal']) && isset($_GET['tanggal_akhir'])) {
-                    $sql_pelaporan = "SELECT * FROM distribusi WHERE tanggal_distribusi BETWEEN '" . $_GET['tanggal_awal'] . "' AND '" . $_GET['tanggal_akhir'] . "' ";
-                } elseif (isset($_GET['bulan']) && isset($_GET['tahun'])) {
-                    $sql_distribusi = "SELECT * FROM distribusi WHERE MONTH(tanggal_distribusi) = " . $_GET['bulan'] . " AND YEAR(tanggal_distribusi) = '" . $_GET['tahun'] . "' ";
-                } elseif (isset($_GET['tahun'])) {
-                    $sql_distribusi = "SELECT * FROM distribusi WHERE  YEAR(tanggal_distribusi) = '" . $_GET['tahun'] . "' ";
-                } else {
-                    $sql_distribusi = "SELECT * FROM distribusi";
-                }
-                $distribusis = Querybanyak($sql_distribusi);
-                foreach ($distribusis as $distribusi) {
-                    $user = Querysatudata("SELECT * FROM user WHERE id_user = " . $distribusi['id_user'] . " ");
-                    $peninjauan = Querysatudata("SELECT * FROM peninjauan WHERE id_peninjauan = " . $distribusi['id_peninjauan'] . "");
-                    $bencana = Querysatudata("SELECT * FROM bencana WHERE id_bencana = " . $peninjauan['id_bencana'] . " ");
-                    $wilayah = Querysatudata("SELECT * FROM wilayah WHERE id_wilayah = " . $peninjauan['id_wilayah'] . " ");
-                ?>
-                    <tr>
-                        <td class="py-1">
-                            <?= $user['nama_user'] ?>
-                        </td>
-                        <td>
-                            <?= $distribusi['tanggal_distribusi'] ?>
-                        </td>
-                        <td>
-                            <?= $bencana['nama_bencana'] ?>
-                        </td>
-                        <td>
-                            <?= $wilayah['kecamatan'] ?> / <?= $wilayah['desa'] ?>
-                        </td>
-                        <td>
-                            <?= $distribusi['status_distribusi'] ?>
-                        </td>
-                    </tr>
-                <?php
-                }
-                ?>
-
-            </tbody>
+    <br>
+    <span class="text-center" style="font-size: 16xpx; font-weight:700;">NOTA DINAS</span>
+        <table style="text-align: left;">
+            <tr>
+                <td>Kepada Yth</td>
+                <td> : </td>
+                <td>BUPATI KUDUS</td>
+            </tr>
+            <tr>
+                <td>Dari</td>
+                <td> : </td>
+                <td>Kepala Badan Penanggukangan Bencana Daerah Kab. Kudus</td>
+            </tr>
+            <tr>
+                <td>Tanggal</td>
+                <td> : </td>
+                <td> <?= TanggalIndonesia(date("Y-m-d")) ?></td>
+            </tr>
+            <tr>
+                <td>Hal</td>
+                <td> : </td>
+                <td>Laporan Distribusi Kejadian Benca dan Kedaruratan di Kab. Kudus <?= $hal ?></td>
+            </tr>
         </table>
+        <br>
 
-      
+        <p style="text-align:justify; ">KEMAJUAN/PROGRESS PENDISTRIBUSIAN BANTUAN KEDARURATAN  BANTUAN TERDAMPAK.</p>
+        <table style="text-align: left;">
+            <tr>
+                <td>a.</td>
+                <td>Penanganan kedaruratan  berjumlah <?= $Penanganan_kedaruratan ?> Kejadian</td>
+            </tr>
+            <tr>
+                <td>b.</td>
+                <td>Bantuan logistik kepada terdampak : <?= $Bantuan_logistik ?> kegiatan</td>
+            </tr>
+        </table>
+        <p style="text-align: justify;">Adapun progress penanganan pendistribusian bantuan sebagai berikut : <p>
+        <div style="align-content: center;">
+            <table class="table table-striped" id="table_data">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Pengatur Distribusi</th>
+                        <th>Tanggal</th>
+                        <th>Bencana</th>
+                        <th>Wilayah</th>
+                        <th>Status Distribusi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $no = 1;
+                    $distribusis = Querybanyak($sql_distribusi);
+                    foreach ($distribusis as $distribusi) {
+                        $user = Querysatudata("SELECT * FROM user WHERE id_user = " . $distribusi['id_user'] . " ");
+                        $peninjauan = Querysatudata("SELECT * FROM peninjauan WHERE id_peninjauan = " . $distribusi['id_peninjauan'] . "");
+                        $bencana = Querysatudata("SELECT * FROM bencana WHERE id_bencana = " . $peninjauan['id_bencana'] . " ");
+                        $wilayah = Querysatudata("SELECT * FROM wilayah WHERE id_wilayah = " . $peninjauan['id_wilayah'] . " ");
+                    ?>
+                        <tr>
+                            <td class="py-1">
+                                <?= $no++ ?>
+                            </td>
+                            <td class="py-1">
+                                <?= $user['nama_user'] ?>
+                            </td>
+                            <td>
+                                <?= $distribusi['tanggal_distribusi'] ?>
+                            </td>
+                            <td>
+                                <?= $bencana['nama_bencana'] ?>
+                            </td>
+                            <td>
+                                <?= $wilayah['kecamatan'] ?> / <?= $wilayah['desa'] ?>
+                            </td>
+                            <td>
+                                <?= $distribusi['status_distribusi'] ?>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
 
-    </div>
-
-    <div style="display: flex; justify-content: space-around">
-      
-        <div></div>
-        <div>
-        <table style="text-align:center;">
-                <tr>
-                    <td style="width: 400px;"></td>
-                    <td>Kepala Pelaksana</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>Badan Penanggulangan daerah</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>Kabupaten Kudus</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>Drs. RINARDI BUDIYANTO</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>Pembina Tk. I</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>NIP. 19690325 198803 1 001</td>
-                </tr>
-
+                </tbody>
             </table>
         </div>
-    </div>
+        <br>
+        <br>
+        <div style="display: flex; justify-content: space-around">
+            <div></div>
+            <div>
+                <table style="text-align:center;">
+                    <tr>
+                        <td style="width: 400px;"></td>
+                        <td>Kepala Pelaksana</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Badan Penanggulangan daerah</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Kabupaten Kudus</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                   
+                    
+                    <tr>
+                        <td></td>
+                        <td>Drs. RINARDI BUDIYANTO</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Pembina Tk. I</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>NIP. 19690325 198803 1 001</td>
+                    </tr>
 
-
-
-
-
-    <script>
-        window.print();
-    </script>
+                </table>
+            </div>
+        </div>
+        <script>
+            window.print();
+        </script>
 </body>
 
 </html>
